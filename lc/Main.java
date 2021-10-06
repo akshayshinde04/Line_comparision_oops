6a0bc8d80054626b1613c03687bf1234f4a7553d
unknown <akshayHVshinde04@gmail.com>
2021-10-06 15:25:15 +0530

    Added: Welcome Message

---
+++ lc/Main.java
@@ -0,0 +1,8 @@
+package com.bl.lc;
+
+public class Main {
+	public static void main(String[] args) {
+		Message msg = new Message();
+		msg.message();
+	}
+}
---
+++ lc/Message.java
@@ -0,0 +1,7 @@
+package com.bl.lc;
+
+public class Message {
+	public void message() {
+		System.out.println("Welcome to Line	Comparison Computation");
+	}
+}
